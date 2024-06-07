c323648ba69676d26cab56998fb72dcf4cef2a63
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-08 04:52:10 +0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import { Container, AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { Container, AppBar, Toolbar, Typography, CssBaseline } from '@material-ui/core';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import Note from './components/Note';
 import NotesProvider from './context/NotesContext';
 
+const theme = createTheme({
+  transitions: {
+    duration: {
+      enteringScreen: 500,
+      leavingScreen: 500,
+    },
+  },
+});
+
 const App: React.FC = () => {
   return (
-    <NotesProvider>
-      <Container>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6">
-              Collaborative Note-Taking App
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Note />
-      </Container>
-    </NotesProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NotesProvider>
+        <Container>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6">
+                Collaborative Note-Taking App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Note />
+        </Container>
+      </NotesProvider>
+    </ThemeProvider>
   );
 };
 
