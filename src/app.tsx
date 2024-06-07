083fe38f30f7bd70248b3264c26a3a04e79f2e7d
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-08 01:40:02 +0600

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,18 @@
+// /frontend/src/App.tsx
+import React from 'react';
+import NotesProvider from './context/NotesContext';
+import Note from './components/Note';
+
+const App: React.FC = () => {
+  return (
+    <NotesProvider>
+      <div className="App">
+        <h1>Collaborative Note-Taking App</h1>
+        <Note />
+      </div>
+    </NotesProvider>
+  );
+};
+
+export default App;
 
