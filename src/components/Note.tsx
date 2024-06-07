0194a3ae51c124e7c288665c68810355d4c6783f
Humaira Azmi <humairaazmi100@gmail.com>
2024-06-08 01:41:00 +0600

    Update Note.tsx

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,1 +1,27 @@
+// /frontend/src/components/Note.tsx
+import React, { useContext } from 'react';
+import { NotesContext } from '../context/NotesContext';
+
+const Note: React.FC = () => {
+  const { notes, addNote, updateNote } = useContext(NotesContext);
+
+  const handleAddNote = () => {
+    addNote();
+  };
+
+  return (
+    <div>
+      <button onClick={handleAddNote}>Add Note</button>
+      {notes.map((note, index) => (
+        <textarea
+          key={index}
+          value={note}
+          onChange={(e) => updateNote(index, e.target.value)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Note;
 
